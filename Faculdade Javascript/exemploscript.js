0095edeeadc17b91b3d6ff8748038e7993ca69a1
João Vitor - Jaau <135184163+JaauSouza@users.noreply.github.com>
2023-08-01 23:11:28 -0300

    att

--- Faculdade Javascript/exemploscript.js
+++ Faculdade Javascript/exemploscript.js
@@ -2,4 +2,4 @@ let nome = prompt ("Qual é o seu nome?") ;
 
 let idade = prompt ("Qual a sua idade ?") ;
 
-alert ("Seu nome é " + nome + " e sua idade é: "  + idade)
+alert ("Seu nome é " + nome + " e sua idade é: "  + idade) ;
